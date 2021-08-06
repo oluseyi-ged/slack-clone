fea73dab4602880d59da10f8b617466c65be9bd5
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-06 14:04:39 +0100

    added user auth with firebase

--- src/App.js
+++ src/App.js
@@ -3,24 +3,34 @@ import Header from "./Header"
 import Sidebar from "./Sidebar"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Chat from "./Chat"
+import { useState } from "react"
+import Login from "./Login"
 
 function App() {
+  const [user, setUser] = useState(null)
+
   return (
     <div className="app">
-      <Router>
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>Welcome</h1>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Router>
+            <Header />
+            <div className="app__body">
+              <Sidebar />
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h1>Welcome</h1>
+                </Route>
+              </Switch>
+            </div>
+          </Router>
+        </>
+      )}
     </div>
   )
 }
---
+++ src/Login.js
@@ -0,0 +1,32 @@
+import React from "react"
+import "./Login.css"
+import { Button } from "@material-ui/core"
+import { auth, provider } from "./firebase"
+
+function Login() {
+  const signIn = (e) => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result)
+      })
+      .catch((error) => {
+        alert(error.message)
+      })
+  }
+
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://thumbs.bfldr.com/at/pl546j-7le8zk-afym5u/v/3033396?expiry=1628858731&fit=bounds&height=800&sig=YzcwM2ZmNDJkOWY3ZmNmY2ExOWIzZGQ0Y2NmN2QxYTYzZGRkYTdjMA%3D%3D&width=1100"
+          alt=""
+        />
+        <h1>Sign in to the slack clone</h1>
+        <Button onClick={signIn}>Sign in with Google</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Login
