75ec6f136b97bc002198264cb14cc046b3eaab86
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-06 11:53:57 +0100

    set up app to retrieve channels from db

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase"
+
 const firebaseConfig = {
   apiKey: "AIzaSyClSeoxvOzpiNNt-qOBhPuU0wzGHLX-5WM",
   authDomain: "slack-clone-54cb9.firebaseapp.com",
@@ -7,3 +9,12 @@ const firebaseConfig = {
   appId: "1:657272414063:web:40567f42d1db27ad3b129a",
   measurementId: "G-XCQXSRB263",
 }
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export { auth, provider }
+export default db
