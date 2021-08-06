58849106197f5ece958e6a180e981cb0f367e332
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-06 19:41:27 +0100

    chat input sends message to db

--- src/Message.js
+++ src/Message.js
@@ -7,7 +7,7 @@ function Message({ message, timestamp, user, userimage }) {
       <img src={userimage} alt="" />
       <div className="message__info">
         <h4>
-          {user}{" "}
+          {user}
           <span className="message__timestamp">
             {new Date(timestamp?.toDate()).toUTCString()}
           </span>
