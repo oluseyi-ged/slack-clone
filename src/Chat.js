518fa2ddae31c04b857a97eb72af179a7ab5b1c0
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-06 12:56:42 +0100

    make chat header details dynamic

--- src/Chat.js
+++ src/Chat.js
@@ -1,18 +1,30 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./Chat.css"
 import { useParams } from "react-router-dom"
 import StarBorderIcon from "@material-ui/icons/StarBorder"
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined"
+import db from "./firebase"
 
 function Chat() {
   const { roomId } = useParams()
+  const [roomDetails, setRoomDetails] = useState(null)
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection("rooms")
+        .doc(roomId)
+        .onSnapshot((snapshot) => setRoomDetails(snapshot.data()))
+    }
+  }, [roomId])
+
+  console.log(roomDetails)
 
   return (
     <div className="chat">
       <div className="chat__header">
         <div className="chat__headerLeft">
           <h4 className="chat__channelName">
-            <strong># general</strong> <StarBorderIcon />
+            <strong>#{roomDetails?.name}</strong> <StarBorderIcon />
           </h4>
         </div>
         <div className="chat__headerRight">
