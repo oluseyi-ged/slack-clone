7da3e80e80a1832746883da3b2cf1229e1a6baa8
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-06 11:10:13 +0100

    sidebar setup and styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css"
 import Header from "./Header"
+import Sidebar from "./Sidebar"
 
 function App() {
   return (
     <div className="app">
       {/* Header */}
       <Header />
+
+      <div className="app__body">
+        <Sidebar />
+      </div>
       {/* Sidebar */}
       {/* React Router => chat screens */}
     </div>
