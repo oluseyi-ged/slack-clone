dec735f37a0b2796ed69ec723692f21ebff9f317
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-06 09:37:24 +0100

    react setup for dummy slack project

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyClSeoxvOzpiNNt-qOBhPuU0wzGHLX-5WM",
+  authDomain: "slack-clone-54cb9.firebaseapp.com",
+  projectId: "slack-clone-54cb9",
+  storageBucket: "slack-clone-54cb9.appspot.com",
+  messagingSenderId: "657272414063",
+  appId: "1:657272414063:web:40567f42d1db27ad3b129a",
+  measurementId: "G-XCQXSRB263",
+}
