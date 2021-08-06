fb44c11518881abdbcfedb188a4e6075468789fb
oluseyi-ged <oluseyi.ged@gmail.com>
2021-08-06 12:25:22 +0100

    channel chatscreen header implemented

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import "./App.css"
 import Header from "./Header"
 import Sidebar from "./Sidebar"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Chat from "./Chat"
 
 function App() {
   return (
     <div className="app">
-      {/* Header */}
-      <Header />
-
-      <div className="app__body">
-        <Sidebar />
-      </div>
-      {/* Sidebar */}
-      {/* React Router => chat screens */}
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   )
 }
